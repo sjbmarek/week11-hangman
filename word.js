e06fabda16fe7e8d7ed2681e726cd5ec4cccab84
Susan Marek <smarek@chaskamn.com>
2018-02-13 14:24:48 -0600

    both constructors working

--- word.js
+++ word.js
@@ -6,20 +6,48 @@ var Letter = require("./Letter");
 //returns a string representing the word.  This should call the function on each letter object (func1) that displays the character or an underscore and concatenate those together.//
 function Word(guess) {
 	this.guess = guess;
+	this.letterThere = false;
 	// this.unknownLetter = unknownLetter;
 	this.unknownWord = [];
+	this.output={};
+
+
+	this.letterPresent = function(){
+		// this.letterThere = false;
+
+		if (output.isPresent) {
+			letterThere = true;
+			console.log("Letter There: " + letterThere);
+		}
+	};
+
 	this.buildWord = function(){
 		for (var i=0; i<selectedWord.length; i++){
 			correctLetter = selectedWord.charAt(i);
 			var output = new Letter(guess);
 			output.let();
+			console.log("IS PRESENT: " + output.isPresent);
 			this.unknownWord.push(output.unknownLetter);
+			// this.letterPresent(output.isPresent);
+			if(output.isPresent){
+				this.letterThere=true;
+			}
 			console.log("From Word Constructor");
 			console.log(this.unknownWord);
+			console.log("Letter There: " + this.letterThere);
+
+	
 
 		};
 		console.log("____________________");
 		console.log(this.unknownWord.join(""));
+	};
+
+	// this.letterPresent = function() {
+	// 	// this.letterThere = false;
+	// 	if (output.isPresent) {
+	// 		letterThere = true;
+	// 	}
 	};
 
 	//takes a character as an argument and calls the guess func2 on each letter object//
@@ -32,7 +60,7 @@ function Word(guess) {
 //takes a character as an argument and calls the guess func2 on each letter object//
 // this.func3();
 // this.func4();
-};
+// };
 
 //  var output = new Word("o");
 //  console.log(output);
